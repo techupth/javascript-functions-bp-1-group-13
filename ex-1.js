f0e01b6b6ffa8db5f71efbcb34c4b21e8c22b8eb
Supakit <book0970403815@hotmail.com>
2023-09-23 01:32:23 +0700

    fix : book send assignment ex-(1-6)

--- ex-1.js
+++ ex-1.js
@@ -1,2 +1,26 @@
 // Exercise #1 Declaring Functions
 // Start coding here
+function add (a,b){
+ const result = a+b
+ return result
+}
+   let add1 = add(10, 5)
+   console.log(add1)
+function subtract (a,b){
+    const result = a-b
+    return result
+   }
+   let subtract1 = subtract(10, 5)
+   console.log(subtract1)
+function multiply (a,b){
+    const result = a*b
+    return result
+   }
+   let multiply1 = multiply(10, 5)
+   console.log(multiply1)
+function divide (a,b){
+    const result = a/b
+    return result
+   }
+   let divide1 = divide(10, 5)
+   console.log(divide1)
--- ex-3.js
+++ ex-3.js
@@ -1,2 +1,22 @@
 // Exercise #3 Declaring Arrow Function
 // Start coding here
+let add = (a,b) => {
+    return a+b
+}
+let add1 = add(1,2)
+console.log(add1)
+let subtract = (a,b) => {
+    return a-b
+}
+let subtract1 = subtract(5, 4)
+console.log(subtract1)
+let multiply = (a,b) => {
+    return a*b
+}
+let multiply1 = multiply(9, 25)
+console.log(multiply1)
+let divide = (a,b) => {
+    return a/b
+}
+let divide1 = divide(500, 5)
+console.log(divide1)
